a83604e47441a8a85cacb467268e0e848004e1b7
RiDominguez <ridominguez@alumnos.uai.cl>
2025-03-30 02:12:05 -0300

    client fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    // Aquí configuramos el proxy para redirigir las peticiones API al backend en Deno
     proxy: {
-      '/api': 'http://localhost:8000', // Redirige las peticiones de la API al servidor Deno
+      '/api': 'http://localhost:8000', // Redirigir todas las solicitudes API al backend Deno
     },
   },
-});
+})
+
